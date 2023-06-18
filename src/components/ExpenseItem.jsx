12c39e1192279660e4c9a3c5b60912a18b891a8a
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-06-18 12:31:17 +0100

    new commit

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import './ExpenseItem.css'
 function ExpenseItem() {
-    
-  return (
-    <div>ExpenseItem</div>
-  )
+
+    return (
+      <>
+        <div className="expense-item">
+          <div>June 18, 2023</div>
+          <div className="expense-item__description">
+            <h2>Car Insurance</h2>
+            <div className="expense-item__price">£300</div>
+          </div>
+        </div>
+      </>
+    );
 }
 
 export default ExpenseItem
