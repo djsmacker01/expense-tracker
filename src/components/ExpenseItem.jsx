c8ca191bc27347656424d466cd9dbf7c5f7d76ab
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-06-18 16:20:06 +0100

    new commit

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import './ExpenseItem.css'
 function ExpenseItem() {
+    const expenseDate = new Date(2023, 5, 18);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 300
 
     return (
       <>
         <div className="expense-item">
-          <div>June 18, 2023</div>
+                <div>{expenseDate.toDateString() }</div>
           <div className="expense-item__description">
-            <h2>Car Insurance</h2>
-            <div className="expense-item__price">£300</div>
+            <h2>{expenseTitle}</h2>
+            <div className="expense-item__price">£{expenseAmount}</div>
           </div>
         </div>
       </>
