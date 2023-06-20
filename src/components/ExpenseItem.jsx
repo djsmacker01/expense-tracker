2d506292e2ff1fd03b876a6f7f0e223806150f53
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-06-20 10:35:59 +0100

    new commit

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import './ExpenseItem.css'
-function ExpenseItem(props) {
-    const {title,amount,date} = props
-    // const expenseDate = new Date(2023, 5, 18);
-    // const expenseTitle = 'Car Insurance';
-    // const expenseAmount = 300
+function ExpenseItem({title,amount,date}) {
+  const month = date.toLocaleString('en-US', { month: 'long' });
+  const day = date.toLocaleString('en-US', { day: '2-digit' });
+  const year= date.getFullYear()
 
     return (
       <>
         <div className="expense-item">
-                <div>{date.toDateString() }</div>
+          <div>{month},</div>
+          <div>{ day}</div>
+          <div>{ year}</div>
+                {/* <div>{date.toLocaleString('en-US',{month:'long'}) }</div> */}
           <div className="expense-item__description">
             <h2>{title}</h2>
             <div className="expense-item__price">£{amount}</div>
