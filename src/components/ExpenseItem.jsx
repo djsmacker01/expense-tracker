80842073a33e47add5d2bb1ff0dd3496d29a7942
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-06-20 10:48:27 +0100

    new commit

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import './ExpenseItem.css'
-function ExpenseItem({title,amount,date}) {
-  const month = date.toLocaleString('en-US', { month: 'long' });
-  const day = date.toLocaleString('en-US', { day: '2-digit' });
-  const year= date.getFullYear()
+import React from "react";
+import "./ExpenseItem.css";
+function ExpenseItem({ title, amount, date }) {
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
 
-    return (
-      <>
-        <div className="expense-item">
-          <div>{month},</div>
-          <div>{ day}</div>
-          <div>{ year}</div>
-                {/* <div>{date.toLocaleString('en-US',{month:'long'}) }</div> */}
-          <div className="expense-item__description">
-            <h2>{title}</h2>
-            <div className="expense-item__price">£{amount}</div>
-          </div>
+  return (
+    <>
+      <div className="expense-item">
+        <div>
+          <div>{month} </div>
+          <div>{day} </div>
+          <div>{year} </div>
         </div>
-      </>
-    );
+
+        {/* <div>{date.toLocaleString('en-US',{month:'long'}) }</div> */}
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">£{amount}</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
