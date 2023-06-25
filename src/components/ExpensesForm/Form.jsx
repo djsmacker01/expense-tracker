fab592ac6f0b4949efbe8b37a36cdd2b075f2b7b
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-06-25 22:02:10 +0100

    new commit

--- src/components/ExpensesForm/Form.jsx
+++ src/components/ExpensesForm/Form.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import '../ExpensesForm/Form.css'
+import React from "react";
+import "../ExpensesForm/Form.css";
 function Form() {
   return (
-      
-          <form action="">
-              
-         </form>
-      
-  )
+    <form action="">
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label htmlFor="">Title</label>
+          <input type="text" />
+        </div>
+
+        <div className="new-expense__control">
+          <label htmlFor="">Amount</label>
+          <input type="number" min="0.01" step="0.01"/>
+        </div>
+
+        <div className="new-expense__control">
+          <label htmlFor="">Date</label>
+          <input type="text" />
+        </div>
+      </div>
+    </form>
+  );
 }
 
-export default Form
+export default Form;
