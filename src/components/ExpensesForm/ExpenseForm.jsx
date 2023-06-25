b89e0f542a19797c36ed64a4461812147f251926
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-06-25 11:22:05 +0100

    new commit

---
+++ src/components/ExpensesForm/ExpenseForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import '../ExpensesForm/ExpensesForm.css'
+function ExpenseForm() {
+  return <div className="new-expense">ExpenseForm</div>;
+}
+
+export default ExpenseForm
---
+++ src/components/ExpensesForm/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../ExpensesForm/Form.css'
+function Form() {
+  return (
+      
+          <form action="">
+              
+         </form>
+      
+  )
+}
+
+export default Form
