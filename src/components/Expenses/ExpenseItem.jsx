8183e8ea35854bed231c1715b077a86ef89e9319
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-06-25 10:54:41 +0100

    new commit

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React,{useState} from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 function ExpenseItem({ title, amount, date }) {
+  const [titles, setTitle] = useState(title) 
+
+  const titleHandler = () => {
+    setTitle('update');
+  }
+
   return (
     <>
       <div className="expense-item">
         <ExpenseDate date={date} />
 
         <div className="expense-item__description">
-          <h2>{title}</h2>
+          <h2>{titles}</h2>
           <div className="expense-item__price">£{amount}</div>
         </div>
+        <button onClick={titleHandler}> click me</button>
       </div>
     </>
   );
