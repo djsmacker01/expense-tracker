6a70608f617568b6eb422f12a6cfa3f56bcf0c16
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-06-30 12:31:07 +0100

    new commit

--- src/components/ExpensesForm/NewExpense.jsx
+++ src/components/ExpensesForm/NewExpense.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import "./Expense.css";
 import ExpenseForm from "./ExpenseForm";
 function NewExpense() {
+
+  const saveExpenseData = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id:Math.random().toString()
+    }
+    console.log(expenseData);
+  }
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSavedData={ saveExpenseData} />
     </div>
   );
 }
