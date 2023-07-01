7ecd657ea5c9965b38aa8aeb29728cf64561135a
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-07-01 17:18:46 +0100

    new commit

--- src/components/Expenses/ExpenseFilter.jsx
+++ src/components/Expenses/ExpenseFilter.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import './ExpenseFilter.css'
 
 function ExpenseFilter() {
+    const expenseFilterHandler = (event) => {
+        console.log(event.target.value)
+    }
   return (
     <div className="expenses-filter">
       <div className="expense-filter__control">
         <label htmlFor="">Filter by year</label>
-        <select name="" id="">
+        <select onChange={expenseFilterHandler}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
